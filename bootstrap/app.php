9a7236e3efd63ea78d33261c4bc3e661cf9d2c3a
mowangjuanzi <baoguoxiao0538@hotmail.com>
2020-10-19 14:14:06 +0800

    优化结构

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,5 @@
 <?php
 
-use Melon\Framework\Contracts\Console\Kernel;
 use Melon\Framework\Foundation\Application;
 
-$app = new Application();
-
-$app->singleton(Kernel::class, \App\Console\Kernel::class);
-
-return $app;
+return new Application(dirname(__DIR__));
--- framework/src/Foundation/Application.php
+++ framework/src/Foundation/Application.php
@@ -8,5 +8,18 @@ use Melon\Framework\Container\Container;
 
 class Application extends Container
 {
+    /**
+     * 基础目录
+     * @var string
+     */
+    protected string $basePath = '';
 
+    /**
+     * 构造器
+     * @param string $basePath
+     */
+    public function __construct(string $basePath)
+    {
+        $this->basePath = $basePath;
+    }
 }
